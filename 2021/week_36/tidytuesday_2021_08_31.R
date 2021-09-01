f98a7d1b0c6912614f6efac4afc6d91a27eb2916
unknown <valenzuela.bernal.s@gmail.com>
2021-08-31 22:08:33 -0500

    This week's tidytuesday contribution

--- 2021/week_36/tidytuesday_2021_08_31.R
+++ 2021/week_36/tidytuesday_2021_08_31.R
@@ -1,6 +1,46 @@
 rm(list=ls(all=T))
 
+library(tidytext)
 library(tidyverse)
+library(showtext)
+
+#font_add_google(name = "Balsamiq Sans", family = "balsamiq")
+showtext_auto()
 
 tuesdata <- tidytuesdayR::tt_load('2021-08-31')
-bird_baths <- tuesdata$bird_baths
+bb <- tuesdata$bird_baths
+
+# We keep only the top 5 type of birds in each bioregion
+b_counts <- bb %>%
+  filter(bird_count >0) %>% 
+  group_by(bird_type, bioregions) %>%
+  summarize(new_bird_count = sum(bird_count)) %>%
+  filter(is.na(bioregions) == F) %>%
+  arrange(desc(new_bird_count)) %>%
+  group_by(bioregions) %>%
+  top_n(n=5)
+
+# Plot the data
+bird_plot <-b_counts %>%
+  ggplot(aes(reorder_within(bird_type, new_bird_count, bioregions),
+             new_bird_count,
+             fill = bioregions)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap( ~bioregions, scales = 'free') +
+  coord_flip(expand = F) +
+  scale_x_reordered() +
+  scale_fill_brewer(palette= "Paired") +
+  labs(title = "Most common Australian birds",
+       subtitle = "Five most common bird species spotted in Australia (2014-15), by bioregion",
+       caption = "Source: Cleary et al, 2016") +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        text = element_text(family = "balsamiq"),
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        plot.caption=element_text(hjust = 0))
+
+ggsave(filename = "tt_plot_20210831.png",
+       plot = bird_plot,
+       device = "png")
