565b3e5d3592798a53814b9a85ab35b8e429d746
unknown <valenzuela.bernal.s@gmail.com>
2021-09-21 21:25:45 -0500

    uploading graphic for tidytuesday wk39, 2021

--- 2021/week_39/tidytuesday_2021_09_21.R
+++ 2021/week_39/tidytuesday_2021_09_21.R
@@ -1,12 +1,49 @@
 rm(list=ls(all=T))
 
+library(gganimate)
+library(showtext)
 library(tidyverse)
+
+#font_add_google("Carrois Gothic SC", "carrois_gothic")
+showtext_auto()
 
 tuesdata <- tidytuesdayR::tt_load('2021-09-21')
 nominees <- tuesdata$nominees
 
-nominees %>% 
+distributors <- c("Netflix", "HBO")
+
+dist_df <-nominees %>% 
   select(distributor, year) %>%
   group_by(distributor, year) %>%
   tally() %>%
-  View()
+  filter(distributor %in% distributors ) %>%
+  arrange(year)
+
+p <- dist_df %>%
+  ggplot(aes(x = year, y = n, color = distributor)) +
+  geom_line(size = 1.1) +
+  geom_point(size = 2) +
+  
+  labs(x = NULL,
+       y = "Number of Emmy nominations",
+       color = NULL,
+       title = "HBO vs Netflix",
+       caption = "Source: emmys.com",
+       subtitle =  "Total number of Emmy nominations per distributor, per year \n {trunc(frame_along)}") +
+  
+  scale_color_manual(values = c("#5822b4", "#E50914")) +
+  theme_minimal() +
+  
+  theme(legend.position= "bottom",
+        plot.title = element_text(color = "#5822b4"),
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        plot.caption=element_text(hjust = 0),
+        text = element_text(family="carrois_gothic")
+        )
+
+anim <- p +
+  transition_reveal(year) +
+  view_follow()
+
+animate(anim, renderer=gifski_renderer("NetflixHBO.gif"))
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 A collection of my contributions to the #tidytuesday challenge.
 
 ## 2021
+### Week 39 (2021-09-21)
+
 ### Week 38 (2021-09-14)
 
 ![Four scatterplots showing how Billboard's number 1 song have gotten louder, more energetic, more danceable and more negative across four Spotify audio features](https://github.com/santiagovalenzuela/tidytuesday/blob/main/2021/week_38/tt_plot_20210914.png)
